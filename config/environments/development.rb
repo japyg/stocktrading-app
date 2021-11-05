7e4439fd0f7dc898a677f4dc929bd178aa55dcc7
Elowen Salas <jackiesalasdev@gmail.com>
2021-11-06 00:16:53 +0800

    still have errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,9 +34,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  # Change mail delvery to either :smtp, :sendmail, :file, :test
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "railscasts.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
