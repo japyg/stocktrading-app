055c531242901c247847727477b14e7a4c34d5ee
Elowen Salas <jackiesalasdev@gmail.com>
2021-11-06 15:27:47 +0800

    error fixed confirmable email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.action_mailer.perform_deliveries = true
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -43,12 +44,12 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "railscasts.com",
+  domain: "smtp.gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
-}
+  user_name: Rails.application.credentials.gmail_credential[:gmail_username],
+  password: Rails.application.credentials.gmail_credential[:gmail_password],
+  openssl_verify_mode: "none"}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
